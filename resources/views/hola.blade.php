3f5419c261919110df675834b57795612d414c46
Juan David Castaño Gómez <desarrollo@fundacionmarialuisa.org>
2020-10-05 19:29:21 -0500

    Nueva vista de prueba llamada hola

---
+++ resources/views/hola.blade.php
@@ -0,0 +1,16 @@
+<h1>Hola soy una prueba</h1>
+
+<table>
+  <thead>
+    <th>Id</th>
+    <th>De</th>
+    <th>Sistema</th>
+  </thead>
+  <tbody>
+    <td>123</td>
+    <td>Alguien</td>
+    <td>Soy un sistema</td>
+  </tbody>
+</table>
+
+<p>soy un parrafo accesskey=""</p>
