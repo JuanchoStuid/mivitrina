8a05d267cea545ffe9ef93da800e0b63df9a141a
Juan David Castaño Gómez <desarrollo@fundacionmarialuisa.org>
2020-11-14 10:42:31 -0500

    Layout, vista, service-block-main, mivitrina

---
+++ resources/views/layouts/service-block-main/service-block-main.blade.php
@@ -0,0 +1,84 @@
+<!-- Container Starts -->
+<div class="container">
+
+    <h1 class="section-title wow fadeIn animated" data-wow-delay=".2s">
+      What We Do
+    </h1>
+    <div class="row">
+      <div class="col-sm-6 col-md-3 text-center">
+      <!-- Service-Block-1 Item Starts -->
+        <div class="service-item wow fadeInUpQuick animated" data-wow-delay=".5s">
+          <div class="icon-wrapper">
+            <i class="icon-Rotate fa-4x">
+            </i>
+          </div>
+          <h2>
+            Responsive Design
+          </h2>
+          <p>
+            I should be incapable of drawing a single stroke at the present moment.
+          </p>
+          <a href="#" class="btn btn-common">
+            Learn More
+          </a>
+        </div><!-- Service-Block-1 Item Ends -->
+      </div>
+
+      <div class="col-sm-6 col-md-3 text-center">
+        <!-- Service-Block-1 Item Starts -->
+        <div class="service-item wow fadeInUpQuick animated" data-wow-delay=".8s">
+          <div class="icon-wrapper">
+            <i class="icon-Backward fa-4x">
+            </i>
+          </div>
+          <h2>
+            Cloud Hosting
+          </h2>
+          <p>
+            I should be incapable of drawing a single stroke at the present moment.
+          </p>
+          <a href="#" class="btn btn-common">
+            Learn More
+          </a>
+        </div><!-- Service-Block-1 Item Ends -->
+      </div>
+
+      <div class="col-sm-6 col-md-3 text-center">
+        <!-- Service-Block-1 Item Starts -->
+        <div class="service-item wow fadeInUpQuick animated" data-wow-delay="1.1s">
+          <div class="icon-wrapper">
+            <i class="icon-Bigger fa-4x">
+            </i>
+          </div>
+          <h2>
+            Digital Marketing
+          </h2>
+          <p>
+            I should be incapable of drawing a single stroke at the present moment.
+          </p>
+          <a href="#" class="btn btn-common">
+            Learn More
+          </a>
+        </div><!-- Service-Block-1 Item Ends -->
+      </div>
+
+      <div class="col-sm-6 col-md-3 text-center">
+        <!-- Service-Block-1 Item Starts -->
+        <div class="service-item  wow fadeInUpQuick animated" data-wow-delay="1.4s">
+          <div class="icon-wrapper">
+            <i class="icon-MagicWand fa-4x">
+            </i>
+          </div>
+          <h2>
+            UX Development
+          </h2>
+          <p>
+            I should be incapable of drawing a single stroke at the present moment.
+          </p>
+          <a href="#" class="btn btn-common">
+            Learn More
+          </a>
+        </div>
+      </div><!-- Service-Block-1 Item Ends -->
+    </div>
+  </div><!-- Container Ends -->
