f33bc0cc59a9a7067b1e0c4a0976680f571dfb3c
Juan David Castaño Gómez <desarrollo@fundacionmarialuisa.org>
2020-11-14 10:38:25 -0500

    Layout, vista, roof, mivitrina

---
+++ resources/views/layouts/header-wrap/roof/roof.blade.php
@@ -0,0 +1,48 @@
+<div class="container">
+
+    <!-- Quick Contacts starts -->
+    <div class="quick-contacts pull-left">
+      <span>
+        <i class="fa fa-map-marker">
+        </i>
+        New South Wales, Sydney
+      </span>
+      <span>
+        <i class="fa fa-mobile-phone">
+        </i>
+        01911787828
+      </span>
+      <span>
+        <i class="fa fa-envelope">
+        </i>
+        email@example.com
+      </span>
+    </div>
+    <!-- Quick Contacts Ends -->
+
+    <!-- Roof Social Links Starts-->
+    <div class="social-links text-center pull-right">
+      <a href="#" class="social-link" data-toggle="tooltip" data-placement="bottom" title="Twitter">
+        <i class="fa fa-twitter">
+        </i>
+      </a>
+      <a href="#" class="social-link" data-toggle="tooltip" data-placement="bottom" title="Facebook">
+        <i class="fa fa-facebook">
+        </i>
+      </a>
+      <a href="#" class="social-link" data-toggle="tooltip" data-placement="bottom" title="Google+">
+        <i class="fa fa-google-plus">
+        </i>
+      </a>
+      <a href="#" class="social-link" data-toggle="tooltip" data-placement="bottom" title="LinkedIn">
+        <i class="fa fa-linkedin">
+        </i>
+      </a>
+      <a href="#" class="social-link" data-toggle="tooltip" data-placement="bottom" title="YouTube">
+        <i class="fa fa-youtube-play">
+        </i>
+      </a>
+    </div>
+    <!-- Roof Social Links Ends-->
+
+</div>
