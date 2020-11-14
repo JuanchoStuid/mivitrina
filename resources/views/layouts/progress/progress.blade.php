71037128b80a43144841ef3bd9f8a818db4ec1d6
Juan David Castaño Gómez <desarrollo@fundacionmarialuisa.org>
2020-11-14 10:41:28 -0500

    Layout, vista, progress, mivitrina

---
+++ resources/views/layouts/progress/progress.blade.php
@@ -0,0 +1,63 @@
+<!-- Progress Inner Starts -->
+<div class="progress-inner">
+    <!-- Container Starts -->
+    <div class="container">
+      <h1 class="section-title wow fadeInUpQuick">
+        Our Work Progress
+      </h1>
+      <!-- Row Starts -->
+      <div class="row">
+        <!-- Progress Items Starts -->
+        <div class="our-progress text-center">
+          <div class="col-md-3 progress-item wow bounceIn" data-wow-delay=".2s">
+            <div class="icon-effect">
+              <i class="icon-Zoom">
+              </i>
+            </div>
+            <h3>
+              Research
+            </h3>
+            <p>
+              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard
+            </p>
+          </div>
+          <div class="col-md-3 progress-item progress-bottom wow bounceIn" data-wow-delay=".5s">
+            <div class="icon-effect">
+              <i class="icon-Brush">
+              </i>
+            </div>
+            <h3>
+              Design
+            </h3>
+            <p>
+              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard
+            </p>
+          </div>
+          <div class="col-md-3 progress-item wow bounceIn" data-wow-delay=".8s">
+            <div class="icon-effect">
+              <i class="icon-CodeSnippet">
+              </i>
+            </div>
+            <h3>
+              Code
+            </h3>
+            <p>
+              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard
+            </p>
+          </div>
+          <div class="col-md-3 progress-item progress-bottom wow bounceIn" data-wow-delay="1.3s">
+            <div class="icon-effect">
+              <i class="icon-Backward">
+              </i>
+            </div>
+            <h3>
+              Launch
+            </h3>
+            <p>
+              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard
+            </p>
+          </div>
+        </div><!-- Progress Items Ends -->
+      </div><!-- Row Ends -->
+    </div><!-- Container Ends -->
+  </div><!-- Progress Inner Ends -->
