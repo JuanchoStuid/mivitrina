0129b6883fc92eca4536be18c9b52f1537b78d63
Juan David Castaño Gómez <desarrollo@fundacionmarialuisa.org>
2020-11-14 10:37:14 -0500

    Layout, vista, footer, mivitrina

---
+++ resources/views/layouts/footer/footer.blade.php
@@ -0,0 +1,118 @@
+<!-- Container Starts -->
+<div class="container">
+    <!-- Row Starts -->
+    <div class="row">
+      <!-- Footer Widget Starts -->
+      <div class="footer-widget col-md-3 col-xs-12 wow fadeIn">
+        <h3 class="small-title">
+          ABOUT Liner
+        </h3>
+        <p>
+          <img src="assets/img/logo.png" alt="">
+        </p>
+        <p>
+          I should be incapable of drawing a single stroke at the present moment. 
+        </p>  
+
+        <div class="social-links-bordered">
+          <a href="#" class="social-link" data-toggle="tooltip" data-placement="bottom" title="Twitter">
+            <i class="fa fa-twitter">
+            </i>
+          </a>
+          <a href="#" class="social-link" data-toggle="tooltip" data-placement="bottom" title="Facebook">
+            <i class="fa fa-facebook">
+            </i>
+          </a>
+          <a href="#" class="social-link" data-toggle="tooltip" data-placement="bottom" title="Google+">
+            <i class="fa fa-google-plus">
+            </i>
+          </a>
+          <a href="#" class="social-link" data-toggle="tooltip" data-placement="bottom" title="LinkedIn">
+            <i class="fa fa-linkedin">
+            </i>
+          </a>
+        </div>          
+      </div><!-- Footer Widget Ends -->
+      
+      <!-- Footer Widget Starts -->
+      <div class="footer-widget col-md-3 col-xs-12 wow fadeIn" data-wow-delay=".2s">
+        <h3 class="small-title">
+          Latest Tweets
+        </h3>
+        <ul class="recent-tweets">
+          <li class="tweet">
+            My <a href="#">@Quora</a>                
+            <span class="tweet-text">
+              answer to What's the best FAQ plugin for WordPress?
+            </span>
+            <a href="#">qr.ae/RFTbIGa</a> 
+            <span class="tweet-date">
+              August 21, 2015 10:29pm
+            </span>
+          </li>
+          
+          
+          <li class="tweet">
+            WPB Advanced FAQ | Probably The Best WordPress FAQ Plugin <a href="#">wpbean.com/wpb-advanced-faq-pr…</a>               
+            <span class="tweet-text">
+              via
+            </span>
+            <a href="#">@wpbean</a> 
+            <span class="tweet-date">
+              August 19, 2015 8:49 am
+            </span>
+          </li>
+          
+          
+        </ul>
+      </div><!-- Footer Widget Ends -->
+
+      <!-- Footer Widget Starts -->
+      <div class="footer-widget col-md-3 col-xs-12 wow fadeIn" data-wow-delay=".5s">
+        <h3 class="small-title">
+          Flicker Gallery
+        </h3>
+        <div class="plain-flicker-gallery">
+          <a href="#" title="Pan Masala"><img src="assets/img/flicker/img1.jpg" alt=""></a>
+          <a href="#" title="Sports Template for Joomla"><img src="assets/img/flicker/img2.jpg" alt=""></a>
+          <a href="" title="Apple Keyboard"><img src="assets/img/flicker/img3.jpg" alt=""></a>
+          <a href="" title="Hard Working"><img src="assets/img/flicker/img4.jpg" alt=""></a>
+          <a href="" title="Smile"><img src="assets/img/flicker/img5.jpg" alt=""></a>
+          <a href="" title="Puzzle"><img src="assets/img/flicker/img6.jpg" alt=""></a>
+        </div>
+      </div><!-- Footer Widget Ends -->
+
+      <!-- Footer Widget Starts -->
+      <div class="footer-widget col-md-3 col-xs-12 wow fadeIn" data-wow-delay=".8s">
+        <h3 class="small-title">
+          Tags
+        </h3>
+        <div class="tagcloud">
+          <a href="#" class="tag-link-201" title="1 topic"><i class="fa fa-tags"></i> Audio Post</a>
+          <a href="#" class="tag-link-197" title="1 topic"><i class="fa fa-tags"></i> Gallery Post</a>
+          <a href="#" class="tag-link-195" title="1 topic"><i class="fa fa-tags"></i> Image Post</a>
+          <a href="#" class="tag-link-211" title="1 topic"><i class="fa fa-tags"></i> life style</a>
+          <a href="#" class="tag-link-204" title="1 topic"><i class="fa fa-tags"></i> Link Post</a>
+          <a href="#" class="tag-link-203" title="1 topic"><i class="fa fa-tags"></i> Quote Post</a>
+          <a href="#" class="tag-link-199" title="1 topic"><i class="fa fa-tags"></i> Video Post</a>
+        </div>
+      </div><!-- Footer Widget Ends -->
+    </div><!-- Row Ends -->
+  </div><!-- Container Ends -->
+  
+  <!-- Copyright -->
+  <div id="copyright">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-9 col-sm-6">
+          <p class="copyright-text">
+            ©  2015 Plain. All right reserved. 
+          </p>
+        </div>
+        <div class="col-md-3  col-sm-6">
+          <p class="copyright-text pull-right">A <a href="http://graygrids.com">GrayGrids</a> Production.</p>             
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- Copyright  End-->
