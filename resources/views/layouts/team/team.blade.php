643281794f262cb2aed4f88456e66b55507a46fc
Juan David Castaño Gómez <desarrollo@fundacionmarialuisa.org>
2020-11-14 10:43:34 -0500

    Layout, vista, team, mivitrina

---
+++ resources/views/layouts/team/team.blade.php
@@ -0,0 +1,200 @@
+<!-- Container Starts -->
+<div class="container">
+
+    <h1 class="section-title wow fadeInDown" data-wow-delay=".5s">
+      Who We Are
+    </h1>
+    <!-- Row Starts -->
+    <div class="row">
+
+      <div class="col-sm-6 col-md-3">
+      <!-- Team Item Starts -->
+        <div class="team-item wow fadeInUpQuick" data-wow-delay="1s">
+          <figure>
+            <img src="assets/img/team/img1.jpg" alt="">
+            <div class="info">
+              <h2>
+                Musharrof Chy
+              </h2>
+              <p>
+                Jr. Frontend Developer
+              </p>
+            </div>
+            <figcaption>
+              <div class="social">
+                <ul>
+                  <li>
+                    <a href="#">
+                      <i class="fa fa-facebook">
+                      </i>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#">
+                      <i class="fa fa-twitter">
+                      </i>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#">
+                      <i class="fa fa-linkedin">
+                      </i>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#">
+                      <i class="fa fa-google-plus">
+                      </i>
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </figcaption>
+          </figure>
+        </div><!-- Team Item Ends -->
+      </div>
+
+
+      <div class="col-sm-6 col-md-3">
+      <!-- Team Item Starts -->
+        <div class="team-item wow fadeInUpQuick" data-wow-delay="1.4s">
+          <figure>
+            <img src="assets/img/team/img2.jpg" alt="">
+            <div class="info">
+              <h2>
+                Justyna Michallek
+              </h2>
+              <p>
+                Business Manager
+              </p>
+            </div>
+            <figcaption>
+              <div class="social">
+                <ul>
+                  <li>
+                    <a href="#">
+                      <i class="fa fa-facebook">
+                      </i>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#">
+                      <i class="fa fa-twitter">
+                      </i>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#">
+                      <i class="fa fa-linkedin">
+                      </i>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#">
+                      <i class="fa fa-google-plus">
+                      </i>
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </figcaption>
+          </figure>
+        </div><!-- Team Item Starts -->
+      </div>
+
+      <div class="col-sm-6 col-md-3">
+        <!-- Team Item Starts -->
+        <div class="team-item wow fadeInUpQuick" data-wow-delay="1.8s">
+          <figure>
+            <img src="assets/img/team/img3.jpg" alt="">
+            <div class="info">
+              <h2>
+                Antonio U.
+              </h2>
+              <p>
+                Intern designer
+              </p>
+            </div>
+            <figcaption>
+              <div class="social">
+                <ul>
+                  <li>
+                    <a href="#">
+                      <i class="fa fa-facebook">
+                      </i>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#">
+                      <i class="fa fa-twitter">
+                      </i>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#">
+                      <i class="fa fa-linkedin">
+                      </i>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#">
+                      <i class="fa fa-google-plus">
+                      </i>
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </figcaption>
+          </figure>
+        </div><!-- Team Item Ends -->
+      </div>
+      
+      <div class="col-sm-6 col-md-3">
+        <!-- Team Item Starts -->
+        <div class="team-item wow fadeInUpQuick" data-wow-delay="2.2s">
+          <figure>
+            <img src="assets/img/team/img4.jpg" alt="">
+            <div class="info">
+              <h2>
+                Darrell S. Allen
+              </h2>
+              <p>
+                Chief Technical Officer
+              </p>
+            </div>
+            <figcaption>
+              <div class="social">
+                <ul>
+                  <li>
+                    <a href="#">
+                      <i class="fa fa-facebook">
+                      </i>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#">
+                      <i class="fa fa-twitter">
+                      </i>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#">
+                      <i class="fa fa-linkedin">
+                      </i>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#">
+                      <i class="fa fa-google-plus">
+                      </i>
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </figcaption>
+          </figure>
+        </div><!-- Team Item Ends -->
+      </div>
+      
+    </div><!-- Row Ends -->
+  </div><!-- Container Ends -->
