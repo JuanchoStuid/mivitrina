a2803e80661bc8082f3a2ce7a0aae64b0f326e86
Juan David Castaño Gómez <desarrollo@fundacionmarialuisa.org>
2020-11-14 10:36:40 -0500

    Layout, vista, cool-facts, mivitrina

---
+++ resources/views/layouts/cool-facts/cool-facts.blade.php
@@ -0,0 +1,60 @@
+<!-- Container Starts -->
+<div class="container">
+    <h1 class="section-title wow fadeInUpQuick">
+      Some Cool Facts
+    </h1>
+    <!-- Row Starts -->
+    <div class="row">
+      <div class="col-md-4">
+        <!-- Fact Block Starts -->
+        <div class="fact-block clearfix wow fadeInUp" data-wow-delay=".3s">
+          <div class="fact-count text-center">
+            <i class="icon-Backward fa-5x">
+            </i>
+            <h3>
+              <span class="counter">
+                478
+              </span>
+            </h3>
+            <h4>
+              Projects Done
+            </h4>
+          </div>
+        </div><!-- Fact Block Ends -->
+      </div>
+      <div class="col-md-4">
+        <!-- Fact Block Starts -->
+        <div class="fact-block clearfix wow fadeInUp" data-wow-delay=".8s">
+          <div class="fact-count text-center">
+            <i class="icon-CodeSnippet fa-5x">
+            </i>
+            <h3>
+              <span class="counter">
+                78545
+              </span>
+            </h3>
+            <h4>
+              Lines Coded
+            </h4>
+          </div>
+        </div><!-- Fact Block Ends -->
+      </div>
+      <div class="col-md-4">
+        <!-- Fact Block Starts -->
+        <div class="fact-block clearfix wow fadeInUp" data-wow-delay="1.1s">
+          <div class="fact-count text-center">
+            <i class="icon-Photo fa-5x">
+            </i>
+            <h3>
+              <span class="counter">
+                398
+              </span>
+            </h3>
+            <h4>
+              Happy Clients
+            </h4>
+          </div>
+        </div><!-- Fact Block Ends -->
+      </div>
+    </div><!-- Row Ends -->
+  </div><!-- Container Ends -->
