c3305242219a108a30a6197e786e88daf86e4af3
Juan David Castaño Gómez <desarrollo@fundacionmarialuisa.org>
2020-11-14 10:20:46 -0500

    Layout, vista, clients, mivitrina

---
+++ resources/views/layouts/clients/clients.blade.php
@@ -0,0 +1,29 @@
+<!-- Container Ends -->
+<div class="container">
+    <h1 class="section-title wow fadeInUpQuick" data-wow-delay=".5s">
+      Clients
+    </h1>
+    <div class="wow fadeInUpQuick" data-wow-delay=".9s">
+      <!-- Row and Scroller Wrapper Starts -->
+      <div class="row" id="clients-scroller">
+        <div class="client-item-wrapper">
+          <img src="assets/img/clients/img1.png" alt="">
+        </div>
+        <div class="client-item-wrapper">
+          <img src="assets/img/clients/img2.png" alt="">
+        </div>
+        <div class="client-item-wrapper">
+          <img src="assets/img/clients/img3.png" alt="">
+        </div>
+        <div class="client-item-wrapper">
+          <img src="assets/img/clients/img4.png" alt="">
+        </div>
+        <div class="client-item-wrapper">
+          <img src="assets/img/clients/img5.png" alt="">
+        </div>
+        <div class="client-item-wrapper">
+          <img src="assets/img/clients/img6.png" alt="">
+        </div>
+      </div><!-- Row and Scroller Wrapper Starts -->
+    </div>
+  </div><!-- Container Ends -->
