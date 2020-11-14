c937bdd41483d59ef21609481f772678c945cda2
Juan David Castaño Gómez <desarrollo@fundacionmarialuisa.org>
2020-11-14 10:44:11 -0500

    Layout, vista, testimonial, mivitrina

---
+++ resources/views/layouts/testimonial/testimonial.blade.php
@@ -0,0 +1,103 @@
+<!-- Carousel Inner Starts -->
+<div class="testimonial-inner">
+    <!-- Container Starts -->
+    <div class="container">
+      <!-- Row Starts -->
+      <div class='row'>
+        <div class="carousel slide" data-ride="carousel" id="testimonial-carousel">
+          <!-- Carousel Items / Quotes -->
+          <div class="carousel-inner">
+            <div class="item active">
+              <blockquote>
+                <div class="col-sm-3 text-center animated zoomIn">
+                  <img class="img-circle" src="assets/img/testimonial/img1.jpg" alt="">
+                </div>
+                <div class="col-sm-9 animated delay-0-5 fadeInUp">
+                  <p>
+                    <i class="fa fa-quote-left fa-2x">
+                    </i>
+                    I am so happy, my dear friend, so absorbed in the exquisite sense of mere tranquil existence, that I neglect my talents. 
+                  </p>
+                  <small>
+                    <span class="name">
+                      <i class="fa fa-user">
+                      </i>
+                      Robert L. Cathcart 
+                    </span>
+                    <span class="company">
+                      <i class="fa fa-building">
+                      </i>
+                      GrayGrids
+                    </span>
+                  </small>
+                </div>
+              </blockquote>
+            </div>
+            
+            <div class="item">
+              <blockquote>
+                <div class="col-sm-3 text-center animated zoomIn">
+                  <img class="img-circle" src="assets/img/testimonial/img2.jpg" alt="">
+                </div>
+                <div class="col-sm-9 animated delay-0-5 fadeInUp">
+                  <p>
+                    <i class="fa fa-quote-left fa-2x">
+                    </i>
+                    I am so happy, my dear friend, so absorbed in the exquisite sense of mere tranquil existence, that I neglect my talents. 
+                  </p>
+                  <small>
+                    <span class="name">
+                      <i class="fa fa-user">
+                      </i>
+                      Robert L. Cathcart 
+                    </span>
+                    <span class="company">
+                      <i class="fa fa-building">
+                      </i>
+                      GrayGrids
+                    </span>
+                  </small>
+                </div>
+              </blockquote>
+            </div>
+            
+            <div class="item">
+              <blockquote>
+                <div class="col-sm-3 text-center animated zoomIn">
+                  <img class="img-circle" src="assets/img/testimonial/img3.jpg" alt="">
+                </div>
+                <div class="col-sm-9 animated delay-0-5 fadeInUp">
+                  <p>
+                    <i class="fa fa-quote-left fa-2x">
+                    </i>
+                    I am so happy, my dear friend, so absorbed in the exquisite sense of mere tranquil existence, that I neglect my talents. 
+                  </p>
+                  <small>
+                    <span class="name">
+                      <i class="fa fa-user">
+                      </i>
+                      Robert L. Cathcart 
+                    </span>
+                    <span class="company">
+                      <i class="fa fa-building">
+                      </i>
+                      GrayGrids
+                    </span>
+                  </small>
+                </div>
+              </blockquote>
+            </div>
+          </div>
+          <!-- Carousel Buttons Next/Prev -->
+          <a data-slide="prev" href="#testimonial-carousel" class="left carousel-control">
+            <i class="fa fa-chevron-left">
+            </i>
+          </a>
+          <a data-slide="next" href="#testimonial-carousel" class="right carousel-control">
+            <i class="fa fa-chevron-right">
+            </i>
+          </a>
+        </div>
+      </div><!-- Row Ends -->
+    </div><!-- Container Ends -->
+  </div><!-- Testimonial Ends -->
