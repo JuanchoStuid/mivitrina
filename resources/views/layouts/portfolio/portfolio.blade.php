2aabce671e778561fb8c0f16ab562462014b41e3
Juan David Castaño Gómez <desarrollo@fundacionmarialuisa.org>
2020-11-14 10:40:17 -0500

    Layout, vista, portfolio, mivitrina

---
+++ resources/views/layouts/portfolio/portfolio.blade.php
@@ -0,0 +1,123 @@
+
+      <!-- Container Starts -->
+      <div class="container">
+        <h1 class="section-title wow fadeInUpQuick">
+          Portfolio 
+        </h1>
+      </div><!-- Container Ends -->
+      
+      <!-- Portfolio Controller/Buttons -->
+      <div class="controls text-center">
+        <a class="filter btn btn-common" data-filter="all">
+          All 
+        </a>
+        <a class="filter btn btn-common" data-filter=".branding">
+          Branding 
+        </a>
+        <a class="filter btn btn-common" data-filter=".marketing">
+          Marketing
+        </a>
+        <a class="filter btn btn-common" data-filter=".planning">
+          Planning 
+        </a>
+        <a class="filter btn btn-common" data-filter=".research">
+          Research 
+        </a>
+      </div>
+      <!-- Portfolio Controller/Buttons Ends-->
+
+
+      <!-- Portfolio Items Wrapper Starts-->
+      <div id="portfolio-items" class="portfolio-items text-center">
+
+        <!-- Portfolio Item Starts -->
+        <div class="mix marketing planning">
+          <figure class="effect-layla">
+            <img src="assets/img/portfolio/img1.jpg">
+            <figcaption>
+              <h3>
+                Ornaments with love
+              </h3>
+              <div class="portfolio-category">
+                <span>Marketing</span>, <span>Planning</span>
+              </div>
+            </figcaption>
+          </figure>
+        </div><!-- Portfolio Item Ends -->
+      
+        <!-- Portfolio Item Starts -->
+        <div class="mix branding planning">
+          <figure class="effect-layla">
+            <img src="assets/img/portfolio/img2.jpg">
+            <figcaption>
+              <h3>
+                Yes i love technolog
+              </h3>
+              <div class="portfolio-category">
+                <span>Branding</span>, <span>Planning</span>   
+              </div>
+            </figcaption>
+          </figure>
+        </div><!-- Portfolio Item Ends -->
+
+        <!-- Portfolio Item Starts -->
+        <div class="mix branding research">
+          <figure class="effect-layla">
+            <img src="assets/img/portfolio/img3.jpg">
+            <figcaption>
+              <h3>
+                Love to take differe
+              </h3>
+              <div class="portfolio-category">
+                <span>Branding</span>, <span>Research</span>   
+              </div>
+            </figcaption>
+          </figure>
+        </div><!-- Portfolio Item Ends -->
+
+        <!-- Portfolio Item Starts -->
+        <div class="mix marketing research">
+          <figure class="effect-layla">
+            <img src="assets/img/portfolio/img4.jpg">
+            <figcaption>
+              <h3>
+                Working With Differe
+              </h3>
+              <div class="portfolio-category">
+                <span>Branding</span>, <span>Research</span>   
+              </div>
+            </figcaption>
+          </figure>
+        </div><!-- Portfolio Item Ends -->
+
+        <!-- Portfolio Item Starts -->
+        <div class="mix marketing planning">
+          <figure class="effect-layla">
+            <img src="assets/img/portfolio/img5.jpg">
+            <figcaption>
+              <h3>
+                Love to take differe
+              </h3>
+              <div class="portfolio-category">
+                <span>Branding</span>, <span>Research</span>   
+              </div>
+            </figcaption>
+          </figure>
+        </div><!-- Portfolio Item Ends -->
+
+        <!-- Portfolio Item Starts -->
+        <div class="mix planning research">
+          <figure class="effect-layla">
+            <img src="assets/img/portfolio/img6.jpg">
+            <figcaption>
+              <h3>
+                Working With Differe
+              </h3>
+              <div class="portfolio-category">
+                <span>Branding</span>, <span>Research</span>   
+              </div>
+            </figcaption>
+          </figure>
+        </div><!-- Portfolio Item Ends -->
+        
+      </div><!-- Portfolio Items Wrapper Ends-->
