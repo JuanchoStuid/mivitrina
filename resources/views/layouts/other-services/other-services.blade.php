41f7dbd52b080c207a25ca730e5fc2fa6abbadd7
Juan David Castaño Gómez <desarrollo@fundacionmarialuisa.org>
2020-11-14 10:39:43 -0500

    Layout, vista, other-services, mivitrina

---
+++ resources/views/layouts/other-services/other-services.blade.php
@@ -0,0 +1,51 @@
+<div class="container">
+    <!-- Container Starts -->
+      <h1 class="section-title wow fadeInUp">
+        Other Services
+      </h1>
+      <div class="row">
+
+        <!-- Other Service Item Wrapper Starts -->
+        <div class="col-sm-6 col-md-6">
+          <article class="other-service-item wow fadeInUp" data-wow-delay=".5s">
+            <div class="icon">
+              <i class="icon-Palette icon-medium"></i>
+            </div>              
+            <div class="service-content">
+              <h3>50+ Pages and Elements</h3>
+              <p>
+                Vestibulum ante ipsum primis in fauc ibus orci luctus etultrices. Lorem ipsum dolor sit.
+              </p>
+            </div>
+          </article>
+          <article class="other-service-item wow fadeInUp" data-wow-delay=".8s">
+            <div class="icon">
+              <i class="icon-Eyeunhide icon-medium"></i>
+            </div>
+            <div class="service-content">
+              <h3>Clean and Creative UI</h3>
+              <p>
+                Vestibulum ante ipsum primis in fauc ibus orci luctus etultrices. Lorem ipsum dolor sit.
+              </p>
+            </div>
+          </article>
+          <article class="other-service-item no-gap-bottom  wow fadeInUp" data-wow-delay="1.2s">
+            <div class="icon">
+              <i class="icon-Paintbucket icon-medium"></i>
+            </div>
+            <div class="service-content">
+              <h3>Unlimited Colors</h3>
+              <p>
+                Vestibulum ante ipsum primis in fauc ibus orci luctus etultrices. Lorem ipsum dolor sit.
+              </p>
+            </div>
+          </article>
+        </div><!-- Other Service Item Wrapper Ends -->
+
+        <!-- Other Service Image -->
+        <div class="col-sm-6 com-md-6 wow fadeInRightQuick" data-wow-delay=".2s">
+          <img src="assets/img/about/other-services.png" class="img-responsive" alt="">
+        </div><!-- Other Service Image Ends-->
+
+      </div>
+    </div><!-- Container Ends -->
