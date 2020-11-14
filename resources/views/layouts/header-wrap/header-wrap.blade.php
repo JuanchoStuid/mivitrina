6d98599289df04c2ac5a13a225711062b0135b30
Juan David Castaño Gómez <desarrollo@fundacionmarialuisa.org>
2020-11-14 10:39:06 -0500

    Layout, vista, header-wrap, mivitrina

---
+++ resources/views/layouts/header-wrap/header-wrap.blade.php
@@ -0,0 +1,10 @@
+<!-- Roof area starts -->
+<div id="roof" class="hidden-xs">
+    @include('layouts.header-wrap.roof.roof')
+</div>
+<!-- Roof area Ends -->
+
+<!-- Header area starts -->
+<section id="header">
+    @include('layouts.header-wrap.header.header')
+</section><!-- Main Slider Section End-->      
