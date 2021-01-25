4e159d76ff240893102af93f3382d76b30200e2d
Juan David Castaño Gómez <desarrollo@fundacionmarialuisa.org>
2021-01-24 20:28:55 -0500

    Blade, dinamico, clientes

--- resources/views/layouts/clients/clients.blade.php
+++ resources/views/layouts/clients/clients.blade.php
@@ -6,24 +6,11 @@
     <div class="wow fadeInUpQuick" data-wow-delay=".9s">
       <!-- Row and Scroller Wrapper Starts -->
       <div class="row" id="clients-scroller">
-        <div class="client-item-wrapper">
-          <img src="assets/img/clients/img1.png" alt="">
-        </div>
-        <div class="client-item-wrapper">
-          <img src="assets/img/clients/img2.png" alt="">
-        </div>
-        <div class="client-item-wrapper">
-          <img src="assets/img/clients/img3.png" alt="">
-        </div>
-        <div class="client-item-wrapper">
-          <img src="assets/img/clients/img4.png" alt="">
-        </div>
-        <div class="client-item-wrapper">
-          <img src="assets/img/clients/img5.png" alt="">
-        </div>
-        <div class="client-item-wrapper">
-          <img src="assets/img/clients/img6.png" alt="">
-        </div>
+        @foreach ($clients as $cli)
+          <div class="client-item-wrapper">
+            <img src="{{ $cli->cli_div_img_src }}" alt="{{ $cli->cli_img_alt }}">
+          </div>
+        @endforeach
       </div><!-- Row and Scroller Wrapper Starts -->
     </div>
   </div><!-- Container Ends -->
