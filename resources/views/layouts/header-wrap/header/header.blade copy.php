a3cfe5765e3ca22c6d7396938c847278579a4e9f
Juan David Castaño Gómez <desarrollo@fundacionmarialuisa.org>
2021-01-24 20:29:07 -0500

    Copia, header

---
+++ resources/views/layouts/header-wrap/header/header.blade copy.php
@@ -0,0 +1,684 @@
+ <!-- Nav Menu with Logo area starts -->
+ <div class="logo-menu">
+
+  <!-- Navbar Starts -->
+  <nav class="navbar navbar-default navbar-plain" role="navigation" data-spy="affix" data-offset-top="50">
+    <div class="container">
+
+      <!-- Brand and toggle get grouped for better mobile display -->
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar">
+        </button>
+        <a class="navbar-brand" href="index.html">
+          <img src="assets/img/logo.png" alt="PLAIN HTML5 Template">
+        </a>
+      </div>
+      <!-- Brand and toggle menu for mobile ends -->
+
+      <!-- Navbar Starts -->
+      <div class="collapse navbar-collapse" id="navbar">
+        <ul class="nav navbar-nav animated-nav navbar-right">
+          <li class="active dropdown dropdown-toggle">
+            <a href="#" data-toggle="dropdown">
+               Home
+            </a>                    
+            <ul class="dropdown-menu">
+              <li class="active">
+                <a href="index.html">
+                  Home v1
+                </a>
+              </li>   
+              <li>
+                <a href="index-1.html">
+                  Home v2
+                </a>
+              </li>                   
+            </ul>                    
+          </li>                                    
+          <li class="dropdown dropdown-toggle">
+            <a href="#" data-toggle="dropdown">
+              Pages
+            </a>
+            <ul class="dropdown-menu">
+              <li class="dropdown">
+                <a href="#">
+                  About Us
+                </a>
+                <ul class="dropdown-menu sub-menu">
+                  <li>
+                    <a href="about.html">
+                      About us 1
+                    </a>
+                  </li>
+                  <li>
+                    <a href="about2.html">
+                      About us 2
+                    </a>
+                  </li>
+                </ul>
+              </li>
+              <li class="dropdown">
+                <a href="services.html">
+                  Services
+                </a>
+                <ul class="dropdown-menu sub-menu">
+                  <li>
+                    <a href="services.html">
+                      services 1
+                    </a>
+                  </li>
+                  <li>
+                    <a href="services2.html">
+                      Services 2
+                    </a>
+                  </li>
+                </ul>
+              </li>
+              <li class="dropdown">
+                <a href="team.html">
+                  Our Team
+                </a>
+                <ul class="dropdown-menu sub-menu">
+                  <li>
+                    <a href="team1.html">
+                      Team Page 1
+                    </a>
+                  </li>
+                  <li>
+                    <a href="team2.html">
+                      Team Page 2
+                    </a>
+                  </li>
+                </ul>
+              </li>
+              <li>
+                <a href="pricing.html">
+                  Pricing Tables
+                </a>
+              </li>
+               <li>
+                <a href="faq.html">
+                  FAQ Page
+                </a>
+              </li> 
+              <li class="dropdown">
+                <a href="#">
+                  Sidebar
+                </a>
+                <ul class="dropdown-menu sub-menu">
+                  <li>
+                    <a href="blog-full.html">
+                      Full width
+                    </a>
+                  </li>
+                  <li>
+                    <a href="blog-sidebar.html">
+                      Left Sidebar
+                    </a>
+                  </li>
+                  <li>
+                    <a href="sidebar-blog.html">
+                      Right Sidebar
+                    </a>
+                  </li>
+                </ul>
+              </li>                      
+              <li>
+                <a href="404.html">
+                  Page 404
+                </a>
+              </li>    
+            </ul>
+          </li>             
+          <li class="dropdown dropdown-toggle">
+            <a href="#" data-toggle="dropdown">
+              Elements 
+            </a>
+            
+            <ul class="dropdown-menu">
+              <li>
+                <a href="accordions.html">
+                  Accordions
+                </a>
+              </li>
+              <li>
+                <a href="buttons.html">
+                  Buttons
+                </a>
+              </li>
+              <li>
+                <a href="social-icons.html">
+                  Social Icons
+                </a>
+              </li>
+              <li>
+                <a href="map.html">
+                  Google Map
+                </a>
+              </li>
+              <li>
+                <a href="media.html">
+                  Media
+                </a>
+              </li>
+              <li>
+                <a href="progress.html">
+                  Progress Bar
+                </a>
+              </li>
+              <li>
+                <a href="portfolio.html">
+                  Portfolio
+                </a>
+              </li>
+              <li>
+                <a href="carousel.html">
+                  Carousel Slider
+                </a>
+              </li>
+              <li>
+                <a href="pricing.html">
+                  Pricing Tables
+                </a>
+              </li>
+              <li>
+                <a href="notification.html">
+                  Notification
+                </a>
+              </li>
+              <li>
+                <a href="tabs.html">
+                  Tabs
+                </a>
+              </li>                        
+            </ul>                   
+            
+          </li>
+          <li class="dropdown dropdown-toggle">
+            <a href="#" data-toggle="dropdown">
+              Portfolio
+            </a>
+            
+            <ul class="dropdown-menu">
+              <li>
+                <a href="portfolio-col-3.html">
+                  Portfolio (3 column)
+                </a>
+              </li>
+              <li>
+                <a href="portfolio-col-4.html">
+                  Portfolio (4 column)
+                </a>
+              </li>
+              <li>
+                <a href="portfolio-item.html">
+                  Single Project
+                </a>
+              </li>
+              
+            </ul>
+            
+          </li>
+
+          <li class="dropdown dropdown-toggle">
+            <a href="#" data-toggle="dropdown">
+              Contact Us
+            </a>
+            
+            <ul class="dropdown-menu">
+              <li>
+                <a href="contact1.html">
+                  Contact us Page 1
+                </a>
+              </li>
+              <li>
+                <a href="contact2.html">
+                  Contact us Page 2
+                </a>
+              </li>                      
+            </ul>
+            
+          </li>
+          
+          <li class="dropdown dropdown-toggle">
+            <a href="#" data-toggle="dropdown">
+              Blog
+            </a>
+            
+            <ul class="dropdown-menu">
+              <li>
+                <a href="sidebar-blog.html">
+                  Blog View
+                </a>
+              </li>
+              <li>
+                <a href="blog-single.html">
+                  Single Post
+                </a>
+              </li>
+              <li>
+                <a href="imagepost.html">
+                  Image Post
+                </a>
+              </li>
+              <li>
+                <a href="gallery.html">
+                  Gallery Post
+                </a>
+              </li>
+              <li>
+                <a href="video.html">
+                  Video Post
+                </a>
+              </li>
+              <li>
+                <a href="quote.html">
+                  Quote Post
+                </a>
+              </li>   
+            </ul>                    
+          </li>
+          <li class="dropdown dropdown-toggle">
+            <a href="#" data-toggle="dropdown">
+              Shop
+            </a>                    
+            <ul class="dropdown-menu">
+              <li>
+                <a href="shop.html">
+                  Shop Front
+                </a>
+              </li>
+              <li>
+                <a href="single-product.html">
+                  Single Products
+                </a>
+              </li>
+              <li>
+                <a href="shop-category.html">
+                  Category
+                </a>
+              </li>   
+            </ul>                    
+          </li>
+          
+          <!-- Search in right of nav -->
+          <li class="search">
+            <a href="#" class="open-search">
+              <i class="fa fa-search">
+              </i>
+            </a>
+          </li>
+          <!-- Search Ends -->
+          
+        </ul>
+        
+        <!-- Form for navbar search area -->
+        <form class="full-search">
+          <div class="container">
+            <input type="text" placeholder="Type to Search">
+            <a href="#" class="close-search">
+              <span class="fa fa-times fa-2x">
+              </span>
+            </a>
+          </div>
+        </form>
+        <!-- Search form ends -->
+        
+      </div>
+    </div>
+
+    <!-- Mobile Menu Start -->
+    <ul class="wpb-mobile-menu">
+      <li class="active">
+        <a href="index.html">
+          Home
+        </a>
+      </li>                                    
+      <li>
+        <a href="#">
+          Pages
+        </a>
+        <ul>
+          <li>
+            <a href="#">
+              About Us
+            </a>
+            <ul>
+              <li>
+                <a href="about.html">
+                  About us 1
+                </a>
+              </li>
+              <li>
+                <a href="about2.html">
+                  About us 2
+                </a>
+              </li>
+            </ul>
+          </li>
+          <li>
+            <a href="services.html">
+              Services
+            </a>
+            <ul>
+              <li>
+                <a href="services.html">
+                  services 1
+                </a>
+              </li>
+              <li>
+                <a href="services2.html">
+                  Services 2
+                </a>
+              </li>
+            </ul>
+          </li>
+          <li>
+            <a href="team.html">
+              Our Team
+            </a>
+            <ul>
+              <li>
+                <a href="team1.html">
+                  Team Page 1
+                </a>
+              </li>
+              <li>
+                <a href="team2.html">
+                  Team Page 2
+                </a>
+              </li>
+            </ul>
+          </li>
+          <li>
+            <a href="pricing.html">
+              Pricing Tables
+            </a>
+          </li>
+           <li>
+            <a href="faq.html">
+              FAQ Page
+            </a>
+          </li> 
+          <li>
+            <a href="#">
+              Sidebar
+            </a>
+            <ul>
+              <li>
+                <a href="blog-full.html">
+                  Full width
+                </a>
+              </li>
+              <li>
+                <a href="blog-sidebar.html">
+                  Left Sidebar
+                </a>
+              </li>
+              <li>
+                <a href="sidebar-blog.html">
+                  Right Sidebar
+                </a>
+              </li>
+            </ul>
+          </li>                      
+          <li>
+            <a href="404.html">
+              Page 404
+            </a>
+          </li>    
+        </ul>
+      </li>             
+      <li>
+        <a href="#">
+          Elements 
+        </a>
+        
+        <ul>
+          <li>
+            <a href="accordions.html">
+              Accordions
+            </a>
+          </li>
+          <li>
+            <a href="buttons.html">
+              Buttons
+            </a>
+          </li>
+          <li>
+            <a href="social-icons.html">
+              Social Icons
+            </a>
+          </li>
+          <li>
+            <a href="map.html">
+              Google Map
+            </a>
+          </li>
+          <li>
+            <a href="media.html">
+              Media
+            </a>
+          </li>
+          <li>
+            <a href="progress.html">
+              Progress Bar
+            </a>
+          </li>
+          <li>
+            <a href="portfolio.html">
+              Portfolio
+            </a>
+          </li>
+          <li>
+            <a href="carousel.html">
+              Carousel Slider
+            </a>
+          </li>
+          <li>
+            <a href="pricing.html">
+              Pricing Tables
+            </a>
+          </li>
+          <li>
+            <a href="notification.html">
+              Notification
+            </a>
+          </li>
+          <li>
+            <a href="tabs.html">
+              Tabs
+            </a>
+          </li>                        
+        </ul>                   
+        
+      </li>
+      <li>
+        <a href="#">
+          Portfolio
+        </a>
+        
+        <ul>
+          <li>
+            <a href="portfolio-col-3.html">
+              Portfolio (3 column)
+            </a>
+          </li>
+          <li>
+            <a href="portfolio-col-4.html">
+              Portfolio (4 column)
+            </a>
+          </li>
+          <li>
+            <a href="portfolio-item.html">
+              Single Project
+            </a>
+          </li>
+          
+        </ul>
+        
+      </li>
+
+      <li>
+        <a href="#">
+          Contact Us
+        </a>
+        
+        <ul>
+          <li>
+            <a href="contact1.html">
+              Contact us Page 1
+            </a>
+          </li>
+          <li>
+            <a href="contact2.html">
+              Contact us Page 2
+            </a>
+          </li>                      
+        </ul>
+        
+      </li>
+      
+      <li>
+        <a href="#">
+          Blog
+        </a>
+        
+        <ul>
+          <li>
+            <a href="sidebar-blog.html">
+              Blog View
+            </a>
+          </li>
+          <li>
+            <a href="blog-single.html">
+              Single Post
+            </a>
+          </li>
+          <li>
+            <a href="imagepost.html">
+              Image Post
+            </a>
+          </li>
+          <li>
+            <a href="gallery.html">
+              Gallery Post
+            </a>
+          </li>
+          <li>
+            <a href="video.html">
+              Video Post
+            </a>
+          </li>
+          <li>
+            <a href="quote.html">
+              Quote Post
+            </a>
+          </li>   
+        </ul>                    
+      </li>    
+      <li>
+        <a href="#">
+          Shop
+        </a>                    
+        <ul>
+          <li>
+            <a href="shop.html">
+              Shop Front
+            </a>
+          </li>
+          <li>
+            <a href="single-product.html">
+              Single Products
+            </a>
+          </li>
+          <li>
+            <a href="shop-category.html">
+              Category
+            </a>
+          </li>   
+        </ul>                    
+      </li>      
+    </ul>
+    <!-- Mobile Menu End -->
+
+  </nav>
+  <!-- Navbar Ends -->
+
+</div>
+<!-- Nav Menu and logo area ends -->
+
+<div id="carousel-area">
+  <div id="carousel-slider" class="carousel slide" data-interval="3000">
+    <!-- Indicators -->
+    <ol class="carousel-indicators">
+      <li data-target="#carousel-slider" data-slide-to="0" class="active">
+      </li>
+      <li data-target="#carousel-slider" data-slide-to="1">
+      </li>
+      <li data-target="#carousel-slider" data-slide-to="2">
+      </li>
+    </ol>
+    <div class="carousel-inner">
+      <!-- Carousel Items Strarts-->
+      <div class="item active" style="background-image: url(assets/img/slider/bg-1.jpg);">
+        <div class="carousel-caption">
+          <h2>
+            WE ARE WEB ARTISANS 
+          </h2>
+          <h3>
+            I should be incapable of drawing a single stroke at the present moment
+          </h3>
+          <a class="btn btn-lg btn-common" href="#">
+            <i class="fa fa-check">
+            </i>
+            Get Started
+          </a>
+        </div>
+      </div>
+      <div class="item" style="background-image: url(assets/img/slider/bg-2.jpg);">
+        <div class="carousel-caption">
+          <h2>
+            REFRESHING DESIGN
+          </h2>
+          <h3>
+            I should be incapable of drawing a single stroke at the present moment
+          </h3>
+          <a class="btn btn-common btn-lg " href="#">
+            <i class="fa fa-coffee">
+            </i>
+            Learn More
+          </a>
+        </div>
+      </div>
+      <div class="item" style="background-image: url(assets/img/slider/bg-3.jpg);">
+        <div class="carousel-caption">
+          <h2>
+            CRAFTED WITH LOVE
+          </h2>
+          <h3>
+            I should be incapable of drawing a single stroke at the present moment
+          </h3>
+          <a class="btn btn-lg btn-common" href="#">
+            <i class="fa fa-coffee">
+            </i>
+            Learn More
+          </a>
+          <a class="btn btn-common btn-lg" href="#">
+            <i class="fa fa-check">
+            </i>
+            Purchase Now
+          </a>
+        </div>
+      </div>
+    </div><!-- Carousel Item Ends -->
+    <a class="left carousel-control" href="#carousel-slider" role="button" data-slide="prev">
+      <i class="fa fa-chevron-left">
+      </i>
+    </a>
+    <a class="right carousel-control" href="#carousel-slider" role="button" data-slide="next">
+      <i class="fa fa-chevron-right">
+      </i>
+    </a>
+  </div>
+</div>
