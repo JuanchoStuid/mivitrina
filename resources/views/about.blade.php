3d4d981784b8cb5c6830f9a861c5329256265096
Juan David Castaño Gómez <30635721+JuanchoNauj@users.noreply.github.com>
2021-04-12 22:39:43 -0500

    Nueva, vista, about, mivitrina

---
+++ resources/views/about.blade.php
@@ -0,0 +1,51 @@
+@section('titleMaestro')
+	| ¿Quiénes Sómos?
+@endsection
+
+@section('style')
+@endsection
+
+@extends('layouts.master-about')
+
+@section('title', '')
+{{-- Inicio de la información --}}
+@section('content-title')
+    ¿Quiénes Sómos?
+@endsection
+@section('content-enlace')
+
+@endsection
+@section('content-icon')
+
+@endsection
+@section('content-call')
+
+@endsection
+@section('content-email')
+    
+@endsection
+{{-- División del content --}}
+@section('content-primario')
+    
+@endsection
+@section('content-about-title')
+    
+@endsection
+@section('content-about-body')
+    
+@endsection
+@section('content-about-mission-vision')
+    
+@endsection
+@section('content-about-fundamentos')
+    
+@endsection
+@section('content-teams')
+    
+@endsection
+@section('content-clients-logos')
+    
+@endsection
+{{-- Fin de la información --}}
+@section('script')
+@endsection
