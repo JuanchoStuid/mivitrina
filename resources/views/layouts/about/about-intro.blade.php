eaf9f7abb89ca34085a9ef071cb89534762058c2
Juan David Castaño Gómez <30635721+JuanchoNauj@users.noreply.github.com>
2021-04-17 13:20:23 -0500

    Actualización de la plantilla about

--- resources/views/layouts/about/about-intro.blade.php
+++ resources/views/layouts/about/about-intro.blade.php
@@ -1,25 +1,27 @@
 <div class="container">
     <div class="row">
-      <div class="col-sm-6 col-md-6">
-        <div class="heading">
-          <h3 class="small-title">
-            {{ $abouts->abo_h3_titulo_principal }}
-          </h3>
-        </div>
-        <div class="the-company wow fadeIn">
-          <p>
-            <img class="{{ $abouts->abo_img_class }}" src="{{ Voyager::image( $abouts->abo_img_src ) }}" alt="{{ $abouts->abo_img_alt }}">
-            {{ $abouts->abo_img_p }}
-            </p>              
-        </div>
-      </div>          
+      @foreach ($abouts as $abou)
+        <div class="col-sm-6 col-md-6">
+          <div class="heading">
+            <h3 class="small-title">
+              {{ $abou->abo_h3_titulo_principal }}
+            </h3>
+          </div>
+          <div class="the-company wow fadeIn">
+            <p>
+              <img class="{{ $abou->abo_img_class }}" src="{{ Voyager::image( $abou->abo_img_src ) }}" alt="{{ $abou->abo_img_alt }}">
+              {{ $abou->abo_img_p }}
+              </p>              
+          </div>
+        </div> 
+      @endforeach         
       <div class="col-sm-6 col-md-6 skill-wrapper wow fadeIn" data-wow-delay=".8s">
         <div class="heading">
           <h3 class="small-title">
-            {{ $abouts->abo_h3_titulo_secundario }}
+            {{ $abouts1->abo_h3_titulo_secundario }}
           </h3>
         </div>
-        @foreach ($skills as $ski)
+        {{-- @foreach ($skills as $ski)
             <div class="progress">
                 <div class="progress-bar" role="progressbar" aria-valuenow="{{ $ski->ski_div_aria_valuenow }}" aria-valuemin="{{ $ski->ski_div_aria_valuemin }}" aria-valuemax="{{ $ski->ski_div_aria_valuemax }}" style="width: {{ $ski->ski_div_style }}">
                 <p>
@@ -30,7 +32,7 @@
                     {{ $ski->ski_span }}
                 </span>
             </div>
-        @endforeach
+        @endforeach --}}
       </div>
     </div>
   </div>
