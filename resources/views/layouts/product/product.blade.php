3f7166445e82dc975e9da0e2c756b7bbe7a4130f
Juan David Castaño Gómez <30635721+JuanchoNauj@users.noreply.github.com>
2021-04-17 19:14:20 -0500

    Actualización de los productos, plantilla, mivitrina

---
+++ resources/views/layouts/product/product.blade.php
@@ -0,0 +1,14 @@
+<div class="container">
+    <div class="row">
+      <div class="col-md-4">
+        <div class="product-details-image">
+            @yield('content-slider')
+        </div>
+      </div>
+      <div class="col-sm-8">
+        <div class="info-panel">
+            @yield('content-info-panel')
+        </div>
+      </div>      
+   </div>
+  </div>
