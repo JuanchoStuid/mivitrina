16abb998dd35e9bca7fbf8e47eb062b256a40f8a
Juan David Castaño Gómez <30635721+JuanchoNauj@users.noreply.github.com>
2021-04-24 16:19:12 -0500

    Actualización imagen

--- resources/views/layouts/portfolio/portfolio.blade.php
+++ resources/views/layouts/portfolio/portfolio.blade.php
@@ -24,7 +24,7 @@
         @foreach ($portfolios as $por)
           <div class="mix {{ $por->por_div_class_mix }}">
             <a class="" href="{{ route("product.show",$por->por_id) }}">
-              <h4>{{ $por->por_h3 }}</h4>
+              {{-- <h4>{{ $por->por_h3 }}</h4> --}}
             <figure class="effect-layla">
               <img src="{{ Voyager::image( $por->por_img_src ) }}" style="width: 100%;">
               <figcaption>
