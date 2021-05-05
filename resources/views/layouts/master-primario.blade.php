b00eb20beccf19e665f306df23bb8949ea770359
Juan David Castaño Gómez <30635721+JuanchoNauj@users.noreply.github.com>
2021-05-04 21:46:23 -0500

    Actualización links, mivitrina

--- resources/views/layouts/master-primario.blade.php
+++ resources/views/layouts/master-primario.blade.php
@@ -26,20 +26,10 @@
     <link rel="stylesheet" type="text/css" href="assets/extras/animate.css">
     <link rel="stylesheet" type="text/css" href="assets/extras/slicknav.css">
 
-    <!-- Color CSS Styles  -->  
+    <!-- Color CSS Styles  -->
+    @include('layouts.links.links')
     
-    <link rel="stylesheet" type="text/css" href="assets/css/colors/default.css" title="default" media="screen" /> 
-{{--     <link rel="stylesheet" type="text/css" href="assets/css/colors/green.css" media="screen" />     
-    <link rel="stylesheet" type="text/css" href="assets/css/colors/lightblue.css" title="lightblue" media="screen" />
-    <link rel="stylesheet" type="text/css" href="assets/css/colors/gray.css" title="gray" media="screen" />
-    <link rel="stylesheet" type="text/css" href="assets/css/colors/coffee.css" title="coffee" media="screen" />    
-    <link rel="stylesheet" type="text/css" href="assets/css/colors/orange.css" title="orange" media="screen" />
-    <link rel="stylesheet" type="text/css" href="assets/css/colors/wisteria.css" title="wisteria" media="screen" />
-    <link rel="stylesheet" type="text/css" href="assets/css/colors/mblue.css" title="mblue" media="screen" />
-    <link rel="stylesheet" type="text/css" href="assets/css/colors/greensea.css" title="greensea" media="screen" />
-    <link rel="stylesheet" type="text/css" href="assets/css/custom.css" title="mivitrina" media="screen" /> --}}
-    
-       @yield('title1')
+    @yield('title1')
     @yield('style')
     
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
