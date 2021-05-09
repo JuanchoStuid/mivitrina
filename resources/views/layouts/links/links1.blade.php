ca88a0eedf43b2f4d14008fcb68678c7e3cf468d
Juan David Castaño Gómez <30635721+JuanchoNauj@users.noreply.github.com>
2021-05-08 20:18:07 -0500

    Nuevos enlaces de estilos y bootstrap

---
+++ resources/views/layouts/links/links1.blade.php
@@ -0,0 +1,18 @@
+<!-- Bootstrap -->
+<link rel="stylesheet" type="text/css" href="assets/css/bootstrap.css">
+<!-- Main Style -->
+<link rel="stylesheet" type="text/css" href="assets/css/main.css">
+<link rel="stylesheet" type="text/css" href="assets/css/custom.css" />
+<!-- Responsive Style -->
+<link rel="stylesheet" type="text/css" href="assets/css/responsive.css">
+<!--Fonts-->
+<link rel="stylesheet" media="screen" href="assets/fonts/font-awesome/font-awesome.min.css">
+<link rel="stylesheet" type="text/css" href="assets/fonts/plain/plain-fonts.css">
+
+ 
+<link rel="stylesheet" type="text/css" href="assets/fonts/lovelo/lovelo.css">
+<!-- Extras -->
+<link rel="stylesheet" type="text/css" href="assets/extras/owl/owl.carousel.css">
+<link rel="stylesheet" type="text/css" href="assets/extras/owl/owl.theme.css">
+<link rel="stylesheet" type="text/css" href="assets/extras/animate.css">
+<link rel="stylesheet" type="text/css" href="assets/extras/slicknav.css">
---
+++ resources/views/layouts/links/links2.blade.php
@@ -0,0 +1,10 @@
+<link rel="stylesheet" type="text/css" href="assets/css/colors/default.css" title="default" media="screen" />
+{{-- <link rel="stylesheet" type="text/css" href="assets/css/colors/green.css" media="screen" />     
+<link rel="stylesheet" type="text/css" href="assets/css/colors/lightblue.css" title="lightblue" media="screen" />
+<link rel="stylesheet" type="text/css" href="assets/css/colors/gray.css" title="gray" media="screen" />
+<link rel="stylesheet" type="text/css" href="assets/css/colors/coffee.css" title="coffee" media="screen" />    
+<link rel="stylesheet" type="text/css" href="assets/css/colors/orange.css" title="orange" media="screen" />
+<link rel="stylesheet" type="text/css" href="assets/css/colors/wisteria.css" title="wisteria" media="screen" />
+<link rel="stylesheet" type="text/css" href="assets/css/colors/mblue.css" title="mblue" media="screen" />
+<link rel="stylesheet" type="text/css" href="assets/css/colors/greensea.css" title="greensea" media="screen" />
+<link rel="stylesheet" type="text/css" href="assets/css/custom.css" title="mivitrina" media="screen" /> --}}
