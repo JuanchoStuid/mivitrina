08a5cd763e46df0ca1aa6ae254734a19b316d8e3
Juan David Castaño Gómez <30635721+JuanchoNauj@users.noreply.github.com>
2021-05-08 20:18:42 -0500

    Actualización del llamado a los enlaces de estilos

--- resources/views/layouts/master-media.blade.php
+++ resources/views/layouts/master-media.blade.php
@@ -6,27 +6,10 @@
     <title>
         Mi Vitrina @yield('titleMaestro')
     </title>
-    <!-- Bootstrap -->
-    <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.css">
-    <!-- Main Style -->
-    <link rel="stylesheet" type="text/css" href="assets/css/main.css">
-    <link rel="stylesheet" type="text/css" href="assets/css/custom.css" />
-    <!-- Responsive Style -->
-    <link rel="stylesheet" type="text/css" href="assets/css/responsive.css">
-    <!--Fonts-->
-    <link rel="stylesheet" media="screen" href="assets/fonts/font-awesome/font-awesome.min.css">
-    <link rel="stylesheet" type="text/css" href="assets/fonts/plain/plain-fonts.css">
-    
-     
-    <link rel="stylesheet" type="text/css" href="assets/fonts/lovelo/lovelo.css">
-    <!-- Extras -->
-    <link rel="stylesheet" type="text/css" href="assets/extras/owl/owl.carousel.css">
-    <link rel="stylesheet" type="text/css" href="assets/extras/owl/owl.theme.css">
-    <link rel="stylesheet" type="text/css" href="assets/extras/animate.css">
-    <link rel="stylesheet" type="text/css" href="assets/extras/slicknav.css">
+    @include('layouts.links.links1')
 
     <!-- Color CSS Styles  -->  
-    @include('layouts.links.links')    
+    @include('layouts.links.links2')    
 
     @yield('title1')
     @yield('style')
